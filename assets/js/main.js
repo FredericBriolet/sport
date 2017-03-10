a6588046fe694aec3436b14bdea2d86d1570e81b
Antonin <antonin.caudron@gmail.com>
2017-03-10 01:08:11 +0100

    Some transitions on timeline elements

--- assets/js/main.js
+++ assets/js/main.js
@@ -21,14 +21,133 @@ function init() {
 }
 
 function initTimeline(el) {
+    var inViewPort = {
+        props: {
+            inViewportActive: {
+                type: Boolean,
+                "default": true
+            },
+            inViewportOnce: {
+                type: Boolean,
+                "default": false
+            },
+            inViewportOffset: {
+                type: Number,
+                "default": 0
+            },
+            inViewportOffsetTop: {
+                type: Number,
+                "default": null
+            },
+            inViewportOffsetBottom: {
+                type: Number,
+                "default": null
+            }
+        },
+        data: function () {
+            return {
+                inViewport: {
+                    now: null,
+                    fully: null,
+                    above: null,
+                    below: null,
+                    listening: false
+                }
+            };
+        },
+        computed: {
+            inViewportOffsetTopComputed: function () {
+                var ref;
+                return (ref = this.inViewportOffsetTop) != null ? ref : this.inViewportOffset;
+            },
+            inViewportOffsetBottomComputed: function () {
+                var ref;
+                return (ref = this.inViewportOffsetBottom) != null ? ref : this.inViewportOffset;
+            },
+            inViewportOffsetComputed: function () {
+                return {
+                    top: this.inViewportOffsetTopComputed,
+                    bottom: this.inViewportOffsetBottomComputed
+                };
+            }
+        },
+        mounted: function () {
+            return this.inViewportInit();
+        },
+        destroyed: function () {
+            return this.removeInViewportHandlers();
+        },
+        watch: {
+            inViewportActive: function (active) {
+                if (active) {
+                    return this.addInViewportHandlers();
+                } else {
+                    return this.removeInViewportHandlers();
+                }
+            },
+            inViewportOffsetComputed: {
+                deep: true,
+                handler: function () {
+                    this.removeInViewportHandlers();
+                    return this.inViewportInit();
+                }
+            }
+        },
+        methods: {
+            inViewportInit: function () {
+                if (this.inViewportActive) {
+                    return this.addInViewportHandlers();
+                }
+            },
+            addInViewportHandlers: function () {
+                if (this.inViewport.listening) {
+                    return;
+                }
+                this.inViewport.listening = true;
+                this.scrollMonitor = scrollMonitor.create(this.$el, this.inViewportOffsetComputed);
+                this.scrollMonitor.on('stateChange', this.updateInViewport);
+                return this.updateInViewport();
+            },
+            removeInViewportHandlers: function () {
+                if (!this.inViewport.listening) {
+                    return;
+                }
+                this.inViewport.listening = false;
+                if (this.scrollMonitor) {
+                    this.scrollMonitor.destroy();
+                }
+                return delete this.scrollMonitor;
+            },
+            updateInViewport: function () {
+                this.inViewport.now = this.scrollMonitor.isInViewport;
+                this.inViewport.fully = this.scrollMonitor.isFullyInViewport;
+                this.inViewport.above = this.scrollMonitor.isAboveViewport;
+                this.inViewport.below = this.scrollMonitor.isBelowViewport;
+                if (this.inViewportOnce && this.inViewport.now) {
+                    return this.removeInViewportHandlers();
+                }
+            }
+        }
+    };
     var article = {
         props: {
             article: Object,
             index: Number
         },
+        data: {
+            // visible: false
+        },
+        mixins: [inViewPort],
         computed: {
+            visible: function () {
+                var self = this;
+                return self.inViewport.now;
+
+            },
+            offset: function () {
+                return -window.innerHeight + 75
+            },
             switchPosition: function () {
-                console.log('sc')
                 var self = this;
                 return self.index % 2 === 0 ? 'article-right' : 'article-left';
             },
@@ -57,23 +176,19 @@ function initTimeline(el) {
                 return path;
             }
         },
-        beforeMount: function () {
-            var self = this;
-            console.log('before-mount');
-            if (this.index === 0) {
-                self.$parent.$data.dates[self.article.date.month.name] = [];
-                console.log(self.article.date.month.name)
-            }
-            // self.$parent.$data.dates[self.article.month.name]
-        },
         template: '' +
         '<article class="timeline-article" :class="this.switchPosition">' +
-        '<div class="article-date"><span class="date">{{ article.date.day }}<sup>th</sup></span></div>' +
+        '<transition name="transition-article-date">' +
+        '<div v-if="visible" class="article-date"><span class="date">{{ article.date.day }}<sup>th</sup></span></div>' +
+        '</transition>' +
         '<div class="article-img-container">' +
-        '<img v-if="article.medias.image" class="article-image" :src="this.article.medias.image" :alt="this.article.title">' +
+            '<transition name="transition-article-image-slide">'+
+        '<img v-show="article.medias.image && visible" class="article-image" :src="this.article.medias.image" :alt="this.article.title">' +
+        '</transition>'+
         '</div>' +
-        '<div class="article-content">' +
-        '<main>' +
+        '<div class="article-content" >' +
+        '<transition name="transition-article-content">' +
+        '<main v-if="visible" >' +
         '<header class="article-header" >' +
         '<svg v-if="this.iconCategoryPath" class="icon article-category"><use :xlink:href="this.iconCategoryPath"/></svg>' +
         '<h2 class="article-title" v-html="this.article.title"></h2>' +
@@ -88,11 +203,12 @@ function initTimeline(el) {
         '</div>' +
         '</footer>' +
         '</main>' +
-        '<aside class="articles-more">' +
-        '<ul class="article-tags">' +
-        '<li class="article-tag" v-for="tag in article.hashtag">{{ tag }}</li>' +
-        '</ul>' +
-        '</aside>' +
+        '</transition>' +
+        // '<aside class="articles-more">' +
+        // '<ul class="article-tags">' +
+        // '<li class="article-tag" v-for="tag in article.hashtag">{{ tag }}</li>' +
+        // '</ul>' +
+        // '</aside>' +
         '</div>' +
         '</article>'
     };
@@ -101,7 +217,15 @@ function initTimeline(el) {
         el: el,
         data: {
             articles: TIMELINE,
-            dates: {}
+            visible: false
+        },
+        mixins: [inViewPort],
+        computed: {
+            visibleComputed: function () {
+                var self = this;
+                console.log('computed')
+                return self.inViewport.now;
+            }
         },
         components: {
             'article-component': article
